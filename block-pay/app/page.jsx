51c6e2ca4650c472fed60d4b4d268261d7b8d0b7
Francis Authority <france-auth@users.noreply.github.com>
2023-08-24 18:04:17 +0100

    "PaymentLinks" imported in page.jsx

--- block-pay/app/page.jsx
+++ block-pay/app/page.jsx
@@ -1,4 +1,4 @@
-import {Navbar, Hero, Advantages} from '@/components/index';
+import {Navbar, Hero, Advantages, PaymentLinks} from '@/components/index';
 import '@/styles/globals.css';
 
 export default function Home() {
