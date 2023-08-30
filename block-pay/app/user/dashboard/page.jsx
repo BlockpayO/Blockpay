96c19c56761bb705bbfb98fbedc3ac01852dfea3
Francis Authority <france-auth@users.noreply.github.com>
2023-08-30 12:07:56 +0100

    userpage

--- block-pay/app/user/dashboard/page.jsx
+++ block-pay/app/user/dashboard/page.jsx
@@ -3,7 +3,6 @@ import SideNav from "@/components"
 const DashBoard = () => {
     return (
         <main>
-            <SideNav/>
             <h1 className="justify-center items-center">
                 Welcome To DashBoard
             </h1>
