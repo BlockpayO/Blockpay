3b79ece3908f3150a809ee1298bf789cc18456aa
Knowledge-JO <megamindtheincredible@gmail.com>
2023-09-21 18:51:36 +0100

    generate receipt coming soon..

--- block-pay/app/user/dashboard/page.jsx
+++ block-pay/app/user/dashboard/page.jsx
@@ -464,7 +464,8 @@ const SearchBar = () => {
         type="search"
         name="search-receipt"
         id=""
-        placeholder="Enter Payment ID"
+        placeholder="Coming soon...."
+        readOnly
         className="w-full text-[13px] py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
       <button
