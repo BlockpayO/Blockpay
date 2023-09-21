f9020f5f504b83571f86521f0dc427abdeba27bb
Knowledge-JO <megamindtheincredible@gmail.com>
2023-09-21 22:08:22 +0100

    udpate connect.js

--- block-pay/app/user/connect.js
+++ block-pay/app/user/connect.js
@@ -6,7 +6,7 @@ import trustModule from "@web3-onboard/trust";
 import ProviderContext from "./context/ProviderContext";
 import { ethers } from "ethers";
 import setContract from "./useContract";
-import {toast} from 'react-toastify'
+import { toast } from "react-toastify";
 
 const HELP = `<svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 704.01 159.42">
 <defs>
@@ -44,7 +44,7 @@ const wcV2InitOptions = {
    */
   projectId: "68e6a9128d33db007d0f2261c25a3bad",
 
-  requiredChains: [ 137, 80001],
+  requiredChains: [137, 80001],
 };
 
 const walletConnect = walletConnectModule(wcV2InitOptions);
@@ -73,7 +73,6 @@ init({
   },
 });
 
-
 const connectWallet = () => {
   const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
   const signerContext = useContext(ProviderContext);
@@ -83,63 +82,28 @@ const connectWallet = () => {
 
   useEffect(() => {
     let ethersProvider;
-  
+
     const handleDisconnect = () => {
       setConnected(false);
       setProvider(undefined); // Set provider to undefined on disconnect
     };
-  
+
     const handleAccountsChanged = (accounts) => {
       // Check if the user disconnected their wallet
       if (accounts.length === 0) {
         handleDisconnect();
       }
     };
-  
+
     if (wallet) {
       try {
         ethersProvider = new ethers.BrowserProvider(wallet.provider, "any");
-  
+
         // Check if ethersProvider is valid before setting it
         if (ethersProvider) {
           setProvider(ethersProvider);
           signerContext.setProvider(ethersProvider);
           setConnected(true);
-  
-          // Check the connected network
-          ethersProvider.getNetwork().then((network) => {
-            const allowedNetworks = ["matic", "mumbai"];
-            if (!allowedNetworks.includes(network.name.toLowerCase())) {
-              setNetworkError(true);
-  
-              // Automatically switch to one of the accepted networks (e.g., Matic Mumbai)
-              if (wallet.provider.isMetaMask) {
-                wallet.provider
-                  .request({
-                    method: "wallet_addEthereumChain",
-                    params: [
-                      {
-                        chainId: "0x13881", // Chain ID for Matic Mumbai
-                        chainName: "Mumbai",
-                        rpcUrls: [`https://polygon-mumbai.infura.io/v3/${INFURA_KEY}`],
-                        nativeCurrency: {
-                          name: "Matic",
-                          symbol: "MATIC",
-                          decimals: 18,
-                        },
-                        blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
-                      },
-                    ],
-                  })
-                  .catch((error) => {
-                    console.error("Error switching network:", error);
-                  });
-              }
-            }
-          });
-  
-          // Listen for accountsChanged event
-          window.ethereum.on("accountsChanged", handleAccountsChanged);
         } else {
           // Handle the case where ethersProvider is undefined
           console.error("Ethers provider is undefined.");
@@ -151,15 +115,7 @@ const connectWallet = () => {
     } else if (!connecting) {
       setConnected(false);
     }
-  
-    return () => {
-      // Cleanup: Remove accountsChanged event listener
-      if (wallet && wallet.provider && window.ethereum.off) {
-        window.ethereum.off("accountsChanged", handleAccountsChanged);
-      }
-    };
   }, [wallet, connecting, signerContext.setProvider]);
-  
 
   if (networkError) {
     // Handle network error state here, e.g., show an error message to the user.
@@ -169,11 +125,3 @@ const connectWallet = () => {
 };
 
 export default connectWallet;
-
-
-
-
-
-
-
-
